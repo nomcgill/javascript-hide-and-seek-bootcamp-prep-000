c765a73efb4e74962501336ae5ccc3fb9fca287e
Nolan McGill <nomcgill@gmail.com>
2018-09-24 01:51:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function getFirstSelector(selector) {
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node');
   var firstNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i) {
-    deepestNode = deepestNode.children[0]
+  for (var i = 0; firstNode.children[i]; i) {
+    firstNode = firstNode.children[0]
   }
-  return deepestNode;
+  return firstNode;
 }
