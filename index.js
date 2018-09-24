27fe7b2156d840c695266b242fac8c8c2ce169fa
Nolan McGill <nomcgill@gmail.com>
2018-09-24 01:49:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getFirstSelector(selector) {
 
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node');
-  var firstNode = grandNode.children:firstChild;
+  var firstNode = grandNode.children:first-child;
   for (var i = 0; !deepestNode.children[i]; i) {
     deepestNode = deepestNode.children[0]
   }
