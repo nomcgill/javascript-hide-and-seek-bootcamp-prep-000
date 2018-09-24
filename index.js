29a1332ac00a9eafa8a2f913a1b510ac5bb7d649
Nolan McGill <nomcgill@gmail.com>
2018-09-24 01:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function getFirstSelector(selector) {
   return document.querySelector('#nested .target');
 }
  function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  var rank = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
   }
 }
  function deepestChild() {
